abfd5e82be7b40c632ab7596c538b9b2a8f150a6
vchikunov <valchikunov@gmail.com>
2017-09-14 00:25:36 +0300

    Added test when elements less than 15

--- VectorAndMapSynchronization/DeleterWithoutPreservationOfOrder.cpp
+++ VectorAndMapSynchronization/DeleterWithoutPreservationOfOrder.cpp
@@ -3,7 +3,7 @@
 
 void DeleterWithoutPreservationOfOrder::DeleteRandomElementsFromVector(std::vector<int>& container)
 {
-	size_t element_count = Generator().GenerateRandomValue(max_elements_for_deletion) + 1;
+	size_t element_count = max_elements_for_deletion > container.size() ? Generator().GenerateRandomValue(container.size()) : Generator().GenerateRandomValue(max_elements_for_deletion) + 1;
 	for (size_t i = 0; i < element_count; i++)
 	{
 		auto index = Generator().GenerateRandomValue(container.size() - 1);
@@ -13,7 +13,7 @@ void DeleterWithoutPreservationOfOrder::DeleteRandomElementsFromVector(std::vect
 
 void DeleterWithoutPreservationOfOrder::DeleteRandomElementsFromMap(std::map<int, int>& container)
 {
-	size_t element_count = Generator().GenerateRandomValue(max_elements_for_deletion) + 1;
+	size_t element_count = max_elements_for_deletion > container.size() ? Generator().GenerateRandomValue(container.size()) : Generator().GenerateRandomValue(max_elements_for_deletion) + 1;
 	for (size_t i = 0; i < element_count; i++)
 	{
 		auto index = Generator().GenerateRandomValue(container.size() - 1);
