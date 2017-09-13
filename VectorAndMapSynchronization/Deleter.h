43abc3b042474ee22c21ff231f5a9fd0e4ae6d7b
vchikunov <valchikunov@gmail.com>
2017-09-13 20:22:37 +0300

    Added deleter with preservation and without preservation

--- VectorAndMapSynchronization/Deleter.h
+++ VectorAndMapSynchronization/Deleter.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "Generator.h"
-#include <vector>
-#include <map>
 class Deleter
 {
 public:
-	static void DeleteRandomElementsFromVector(std::vector<int>& container);
-	static void DeleteRandomElementsFromMap(std::map<int,int>& container);
-
+	virtual void DeleteRandomElementsFromVector(std::vector<int>& container) = 0;
+	virtual void DeleteRandomElementsFromMap(std::map<int,int>& container) = 0;
+	size_t max_elements_for_deletion = 15;
 };
 
--- VectorAndMapSynchronization/VectorAndMapSynchronization.cpp
+++ VectorAndMapSynchronization/VectorAndMapSynchronization.cpp
@@ -4,14 +4,19 @@
 #include "stdafx.h"
 #include "Builder.h"
 #include "Synchronizer.h"
-#include "Deleter.h"
+#include "DeleterWithPreservationOfOrder.h"
+#include "DeleterWithoutPreservationOfOrder.h"
+
+#include <memory>
+
 int main()
 {
 	Builder builder(100);
 	auto sync_vector = builder.GenerateFillVector();
 	auto sync_map = builder.GenerateFillMap();
-	Deleter().DeleteRandomElementsFromVector(sync_vector);
-	Deleter().DeleteRandomElementsFromMap(sync_map);
+	std::unique_ptr<Deleter>deleter(new DeleterWithPreservationOfOrder());
+	deleter->DeleteRandomElementsFromVector(sync_vector);
+	deleter->DeleteRandomElementsFromMap(sync_map);
 	Synchronizer().Synchronize(sync_vector, sync_map);
     return 0;
 }
